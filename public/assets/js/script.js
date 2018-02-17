d313c32f8a9a30405df75e6b2bed6dd736ae8d93
Melodie Chi <chzwzrd@chzwz-2.local>
2018-02-16 17:53:42 -0800

    redesign about page and styling

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,6 @@
 $(() => {
     $('[data-toggle="tooltip"]').tooltip();
+    $("#carousel-example-genereic").carousel();
     $("#hire-me-btn").on('click', function() {
         window.location.href = './contact.html';
     });
